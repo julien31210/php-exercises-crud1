f85fbab370b7fb25136f3692de1ca9a7d6591b13
Julien <julien.linder@gmail.com>
2017-04-21 15:08:27 +0200

    exo 4 et 6 fait

--- index.php
+++ index.php
@@ -6,41 +6,42 @@
 </head>
 <body>
 	<?php 
-		echo "EXO 1: "."<br/>";
-	 	$connect = mysqli_connect('localhost', 'root', 'root', 'colyseum')or die("pb de connexion au serveur");
-		$result = mysqli_query($connect, 'SELECT * FROM clients');
-		While($data = mysqli_fetch_assoc($result)){
-		 	echo $data['lastName']." ";
-		 	echo $data['firstName']."<br/>";
-		 }
-		 echo "<br/>"."EXO 2: ";
-		$result2 = mysqli_query($connect, 'SELECT * FROM showTypes');
-		While($data = mysqli_fetch_assoc($result2)){
-		 	echo "<br/>".$data['type'];
-		 }
+	echo "EXO 1: "."<br/>";
+	$connect = mysqli_connect('localhost', 'root', 'root', 'colyseum')or die("pb de connexion au serveur");
+	$result = mysqli_query($connect, 'SELECT * FROM clients');
+	While($data = mysqli_fetch_assoc($result)){
+		echo $data['lastName']." ";
+		echo $data['firstName']."<br/>";
+	}
+	echo "<br/>"."EXO 2: ";
+	$result2 = mysqli_query($connect, 'SELECT * FROM showTypes');
+	While($data = mysqli_fetch_assoc($result2)){
+		echo "<br/>".$data['type'];
+	}
 
-		 echo "<br/><br/>"."EXO 3: "."<br/>";
-		$result3 = mysqli_query($connect, 'SELECT * FROM clients LIMIT 20');
-		While($data = mysqli_fetch_assoc($result3)){
-			echo $data['lastName'], " ".$data['firstName']."<br/>";
-		}
+	echo "<br/><br/>"."EXO 3: "."<br/>";
+	$result3 = mysqli_query($connect, 'SELECT * FROM clients LIMIT 20');
+	While($data = mysqli_fetch_assoc($result3)){
+		echo $data['lastName'], " ".$data['firstName']."<br/>";
+	}
 
-		echo "<br/><br/>"."Exo4: "."<br/>";
-		$result4 = mysqli_query($connect, 'SELECT * FROM clients WHERE card != 0');
-		While($data = mysqli_fetch_assoc($result4)){
-			echo $data['lastName'], " ".$data['firstName'], " ".$data['cardNumber']."<br/>";
-		}
+	echo "<br/><br/>"."Exo4: "."<br/>";
+	$result4 = mysqli_query($connect, 'SELECT clients.firstName as firstName FROM clients JOIN cards ON clients.cardNumber=cards.cardNumber WHERE cards.cardTypesId=1');
+	While($data = mysqli_fetch_assoc($result4)){
+		echo $data['firstName'], " ".$data['lastName']."<br/>";
+	}
 
-		echo "<br/><br/>"."Exo5: "."<br/>";
-		$result5 = mysqli_query($connect, 'SELECT lastName, firstName FROM clients WHERE lastName  LIKE "M%" ORDER BY lastName ASC');
-		While($data = mysqli_fetch_assoc($result5)){
-			echo $data['lastName']," ".$data['firstName']."<br/>";
-		}
-	 ?>
-		<!--  
+	echo "<br/><br/>"."Exo5: "."<br/>";
+	$result5 = mysqli_query($connect, 'SELECT lastName, firstName FROM clients WHERE lastName  LIKE "M%" ORDER BY lastName ASC');
+	While($data = mysqli_fetch_assoc($result5)){
+		echo $data['lastName']," ".$data['firstName']."<br/>";
+	}
 
-		 mysqli_query($connect, "INSERT INTO Persons(FirstName, LastName, Age)VALUES('Glenn', 'Quagmire', 33)");
-
-		 mysqli_query($connect, "UPDATE login SET column1=value1, column2=value2 WHERE some_column=some_value;"); -->
+	echo "<br/><br/>"."Exo6 "."<br/>";	 
+	$result6 = mysqli_query($connect, 'SELECT * FROM shows ORDER BY title ASC');
+	While($data = mysqli_fetch_assoc($result6)){
+		echo $data['title']," par ".$data['performer'], " le ".$data['date'], " à ".$data['startTime']."<br/>";
+	}
+	?>
 </body>
 </html>
