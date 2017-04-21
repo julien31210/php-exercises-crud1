67512324227f18afd483018582b4ef2fa86fc99f
Julien <julien.linder@gmail.com>
2017-04-21 11:44:33 +0200

    Exo 3 fait

--- index.php
+++ index.php
@@ -2,21 +2,29 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Exo1</title>
+	<title>Exos</title>
 </head>
 <body>
 	<?php 
+		echo "EXO 1: "."<br/>";
 	 	$connect = mysqli_connect('localhost', 'root', 'root', 'colyseum')or die("pb de connexion au serveur");
 		$result = mysqli_query($connect, 'SELECT * FROM clients');
 		While($data = mysqli_fetch_assoc($result)){
 		 	echo $data['lastName']." ";
 		 	echo $data['firstName']."<br/>";
 		 }
-		$result2 = mysqli_query($connect, 'SELECT * FROM shows');
+		 echo "<br/>"."EXO 2: ";
+		$result2 = mysqli_query($connect, 'SELECT * FROM showTypes');
 		While($data = mysqli_fetch_assoc($result2)){
-		 	echo "<br/>".$data['title'].": ";
-		 	echo $data['performer'];
+		 	echo "<br/>".$data['type'];
 		 }
+
+		 echo "<br/><br/>"."EXO 3: "."<br/>";
+		$result3 = mysqli_query($connect, 'SELECT * FROM clients LIMIT 20');
+		While($data = mysqli_fetch_assoc($result3)){
+			echo $data['firstName']."<br/>";
+		}
+
 	 ?>
 		<!--  
 
