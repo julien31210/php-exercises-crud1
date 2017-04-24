05156057e4e9939b15c943f08782385bd104088b
Julien <julien.linder@gmail.com>
2017-04-24 10:10:09 +0200

    Tout finis

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 	<?php 
+	//EXO 1
 	echo "EXO 1: "."<br/>";
 	$connect = mysqli_connect('localhost', 'root', 'root', 'colyseum')or die("pb de connexion au serveur");
 	$result = mysqli_query($connect, 'SELECT * FROM clients');
@@ -13,36 +14,43 @@
 		echo $data['lastName']." ";
 		echo $data['firstName']."<br/>";
 	}
+
+	//EXO 2
 	echo "<br/>"."EXO 2: ";
 	$result2 = mysqli_query($connect, 'SELECT * FROM showTypes');
 	While($data = mysqli_fetch_assoc($result2)){
 		echo "<br/>".$data['type'];
 	}
 
+	//EXO 3
 	echo "<br/><br/>"."EXO 3: "."<br/>";
 	$result3 = mysqli_query($connect, 'SELECT * FROM clients LIMIT 20');
 	While($data = mysqli_fetch_assoc($result3)){
 		echo $data['lastName'], " ".$data['firstName']."<br/>";
 	}
 
+	//EXO 4
 	echo "<br/><br/>"."Exo4: "."<br/>";
 	$result4 = mysqli_query($connect, 'SELECT clients.firstName as firstName FROM clients JOIN cards ON clients.cardNumber=cards.cardNumber WHERE cards.cardTypesId=1');
 	While($data = mysqli_fetch_assoc($result4)){
 		echo $data['firstName'], " ".$data['lastName']."<br/>";
 	}
 
+	//EXO 5
 	echo "<br/><br/>"."Exo5: "."<br/>";
 	$result5 = mysqli_query($connect, 'SELECT lastName, firstName FROM clients WHERE lastName  LIKE "M%" ORDER BY lastName ASC');
 	While($data = mysqli_fetch_assoc($result5)){
 		echo $data['lastName']," ".$data['firstName']."<br/>";
 	}
 
+	//EXO 6
 	echo "<br/><br/>"."Exo6 "."<br/>";	 
 	$result6 = mysqli_query($connect, 'SELECT * FROM shows ORDER BY title ASC');
 	While($data = mysqli_fetch_assoc($result6)){
 		echo $data['title']," par ".$data['performer'], " le ".$data['date'], " à ".$data['startTime']."<br/>";
 	}
 
+	//EXO 7
 	echo "<br/><br/>"."Exo7 "."<br/>";
 	$result7 = mysqli_query($connect, 'SELECT * FROM clients LEFT OUTER JOIN cards ON clients.cardNumber=cards.cardNumber');
 	While($data = mysqli_fetch_assoc($result7)){
@@ -55,11 +63,6 @@
 	}
 	echo"<br/><br/>";
 	}
-// 	Nom : *Nom de famille du client*
-// Prénom : *Prénom du client*
-// Date de naissance : *Date de naissance du client*
-// Carte de fidélité : *Oui (Si le client en possède une) ou Non (s'il n'en possède pas)*
-// Numéro de carte : *Numéro de la carte fidélité du client s'il en possède une.*
 	?>
 </body>
 </html>
